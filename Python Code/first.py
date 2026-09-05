a5c215977b120c96416918cb49985037100c1b2e
Prasun Neupane <prasunneupane14@gmail.com>
2026-09-05 07:59:09 +0545

    Day 1 of learning python for everybody

---
+++ Python Code/first.py
@@ -0,0 +1,9 @@
+print("hello from a file")
+x = 5
+x= x+1 
+print(x)
+# name = input("Enter File:")
+# Reserved Words in Python
+# 1. You cannot use reserved words as variable names/identifiers.
+# IF, ELSE, WHILE, FOR, DEF, CLASS, TRY, EXCEPT, FINALLY, RAISE, IMPORT, FROM, AS, PASS, BREAK, CONTINUE, RETURN, YIELD, GLOBAL, NONLOCAL, ASSERT, LAMBDA, WITH, DEL
+
